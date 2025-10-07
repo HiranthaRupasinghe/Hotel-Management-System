197369544aa36801d9f270f408c4781850d77cd3
Hirantha Rupasinghe <adriyallaka021085@gmail.com>
2025-10-07 23:22:02 +0530

    Added hoteldb.sql database file

---
+++ hoteldb.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE hotel;
+USE hotel;
+Create table users(name varchar(200),
+					email varchar(200),
+                    password varchar(50),
+                    securityQuestion varchar(500),
+                    answer varchar(200),
+                    address varchar(200),
+                    status varchar(20));
+
+create table room(roomNo varchar(10),
+				  roomType varchar(200),
+                  bed varchar(200),
+                  price int,
+                  status varchar(20));
+                  
+create table customer(id int, 
+					  name varchar(200), 
+                      mobileNumber varchar(20), 
+                      nationality varchar(200), 
+                      gender varchar(50), 
+                      email varchar(200), 
+                      idProof varchar(200), 
+                      address varchar(500), 
+                      checkIn varchar(50), 
+                      roomNo varchar(10), 
+                      bed varchar(200), 
+                      roomType varchar(200), 
+                      pricePerDay int(10), 
+                      numberOfDayStay int(10), 
+                      totalAmount varchar(200), 
+                      checkout varchar(50));
